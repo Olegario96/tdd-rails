0f4fc3786e5c97bd81512963ec1d4a996e4a19a3
Gustavo Olegário <gustavo-olegario@hotmail.com>
2018-02-23 14:40:30 +0000

    Learning about classes matchers

--- tutorial/spec/matchers/classes/classes_spec.rb
+++ tutorial/spec/matchers/classes/classes_spec.rb
@@ -1,5 +1,23 @@
 describe 'Classes' do
   it 'be_instance_of'do
+    # Instance of is used to compare exactly the class
     expect(10).to be_instance_of(Integer)
+  end
+
+  it 'be_kind_of' do
+    # Kind of accepts inheritance
+    expect(10).to be_kind_of(Integer)
+  end
+
+  it 'be_a' do
+    expect('ruby').to be_a(String)
+  end
+
+  it 'be_an' do
+    expect(1).to be_an(Integer)
+  end
+
+  it 'respond_to' do
+    expect('ruby').to respond_to(:size)
   end
 end
