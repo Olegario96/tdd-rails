b8d1ca456299b9e8ab9bd0f661ddd3efd2f42f12
Gustavo Olegário <gustavo-olegario@hotmail.com>
2018-02-27 23:15:09 +0000

    Learning about fixtures

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :customers
+
+  it 'Create a customer' do
+    customer = customers(:gustavo)
+    expect(customer.full_name).to eq('Sr. Gustavo Olegario')
+  end
 end
