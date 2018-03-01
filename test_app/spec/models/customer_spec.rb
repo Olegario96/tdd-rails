61dc84c656a9c275c488a70f220a2da895f41fb5
Gustavo Olegário <gustavo-olegario@hotmail.com>
2018-03-01 13:03:07 +0000

    Using inheritance in factory girl

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to eq('Sr. Gustavo Olegario')
   end
 
+  it 'check if Sr. in full_name' do
+    customer = create(:customer)
+    expect(customer.full_name).to start_with('Sr. ')
+  end
+
+  it 'inheritance in factory girl' do
+    customer = create(:customer_not_vip)
+    expect(customer.vip).to be_falsy
+  end
+
   it { expect{ create(:customer) }.to change { Customer.all.size }.by(1) }
 end
