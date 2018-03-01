03606b05279fcf800633ee8884ad835a9dc6f4cf
Gustavo Olegário <gustavo-olegario@hotmail.com>
2018-03-01 16:52:48 +0000

    Associantion with has_many in factory girl

--- test_app/spec/models/order_spec.rb
+++ test_app/spec/models/order_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it 'Order number 1' do
+  it 'belongs to' do
     customer = create(:customer)
     order = create(:order, customer: customer)
     expect(order.customer).to be_kind_of(Customer)
   end
 
-  it 'Multiple orders' do
+  it 'Create list with 3 orders' do
     orders = create_list(:order, 3, description: "Test")
-    puts orders
     expect(orders.size).to eq(3)
+  end
+
+  it 'has_many' do
+    customer = create(:customer_with_orders) #, :with_orders, qtt_orders: 5)
+    expect(customer.orders.size).to eq(3)
   end
 end
