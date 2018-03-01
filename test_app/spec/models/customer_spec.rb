54d071440d63e286f531380c93932ac8e55dfca0
Gustavo Olegário <gustavo-olegario@hotmail.com>
2018-03-01 12:48:53 +0000

    Learning about override and aliases in factory girl

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   it '#full_name' do
-    customer = create(:customer)
-    expect(customer.full_name).to start_with('Sr. ')
+    customer = create(:user, name: 'Gustavo Olegario') #(:customer)
+    expect(customer.full_name).to eq('Sr. Gustavo Olegario')
   end
 
   it { expect{ create(:customer) }.to change { Customer.all.size }.by(1) }
