1bc7ba31303eaf50a79d808460a9a828f041d0f6
Gustavo Olegário <gustavo-olegario@hotmail.com>
2018-03-07 12:23:04 +0000

    Configuring screenshot for capybara

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.feature "Customers", type: :feature do
+RSpec.feature "Customers", type: :feature, js: true do
   it 'visit index page for customers' do
     visit(customers_path)
-    save_and_open_page
+    page.save_screenshot('screenshot.png')
     expect(page).to have_current_path(customers_path)
   end
 end
