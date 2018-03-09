2ac83c8cb14a63c883bf24be9d685a6fd7391168
Gustavo Olegário <gustavo-olegario@hotmail.com>
2018-03-09 20:44:22 +0000

    Creating a customer through API

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Customers", type: :request do
-  describe "GET /customers" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Customers', type: :request do
+  describe 'GET /customers' do
+    it 'works! (now write some real specs)' do
       get customers_path
       expect(response).to have_http_status(200)
     end
 
-    it "index - json" do
+    it 'index - JSON' do
       get '/customers.json'
       expect(response.body).to include_json([
         name: (be_kind_of String),
@@ -15,12 +15,25 @@ RSpec.describe "Customers", type: :request do
       ])
     end
 
-    it "show - json" do
+    it 'show - JSON' do
       get '/customers/1.json'
       expect(response.body).to include_json(
         id: /\d/,
         name: (be_kind_of String),
         email: (be_kind_of String)
+      )
+    end
+
+    it 'create - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+      headers = {'Accept' => 'application/json'}
+      customer_params = attributes_for(:customer)
+      post '/customers', params: { customer: customer_params }, headers: headers
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customer_params.fetch(:name),
+        email: customer_params.fetch(:email)
       )
     end
   end
