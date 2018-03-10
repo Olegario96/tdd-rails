80fdca95a3101ed49c0a45ff5760aa9eca0e1f0c
Gustavo Olegário <gustavo-olegario@hotmail.com>
2018-03-10 15:39:19 +0000

    Creating link to create client

--- tdd_app/spec/features/welcomes_spec.rb
+++ tdd_app/spec/features/welcomes_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Welcome", type: :feature do
   scenario 'Show welcome message' do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Welcome!')
+  end
+
+  scenario 'click link to create new client' do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Create client')
   end
 end
